f4c5d155edc397b9a2221e959aada336325e478e
Lincoln Nowell <lincolnnowell@gmail.com>
2019-04-09 10:35:06 -0500

    Update Input_Parser.cpp

--- Chemisty_table/Input_Parser.cpp
+++ Chemisty_table/Input_Parser.cpp
@@ -28,11 +28,12 @@ public:
 
 class compound {
 
-	//stores elements in vector
+	// stores elements in vector
+	// The compound stores a Variable so it can be turned into a algebriac equation
 public:
 
-	compound(const std::vector<element>& k_elements) :
-	compound_elements(k_elements)
+	compound(const std::vector<element>& k_elements, bool ChangeSide = false) :
+	compound_elements(k_elements), mChangeSide(ChangeSide)
 	{
 	}
 
@@ -41,19 +42,28 @@ public:
 	int size = compound_elements.size();
 	int Constant = 0;
 	char mVariable = ' ';
+	bool mChangeSide = false; // if the compound is right before the equals sign set to true
 
 };
 
 struct equation
 {
 	//stores both the left and right sides of the equation
-	equation(std::vector<compound> Left, std::vector<compound> Right) : left(Left), right(Right){}
+	equation(const std::vector<compound>& Left, const std::vector<compound>& Right) : left(Left), right(Right){}
 
 	std::vector<compound> left, right;
 };
 
-void balance(equation);
-int getConstant(std::map<int, int>);
+struct Variable {
+
+	Variable(char Var = ' ', int Num = 0) : var(Var), constant(Num) {}
+
+	char var = ' ';
+	int constant = 0;
+};
+
+void Turn_into_Algebra_Equation(const equation&);
+Variable Solve_for_Variable(std::string, char,std::vector<Variable>);
 
 int main() {
 
@@ -96,7 +106,7 @@ void parse(std::string line)
 
 		//add the element before to Lefthandside before changing sides
 		if (line.at(character) == '=') {
-			LeftHandSide.push_back(compound(compound_to_be_stored));
+			LeftHandSide.push_back(compound(compound_to_be_stored, true));
 			compound_to_be_stored.clear();
 			left_side = false;
 			continue;
@@ -168,37 +178,56 @@ void parse(std::string line)
 	}
 
 	equation chemical_equation(LeftHandSide, RightHandSide);
-	balance(chemical_equation);
+	Turn_into_Algebra_Equation(chemical_equation);
 }
 
-void balance(equation equation_to_balance) {
+void Turn_into_Algebra_Equation(const equation& equation_to_balance) {
 	
 	std::vector<compound> total;
 	
 	//combine the left and right sides
-	for(auto& Cmpd : equation_to_balance.left) {total.push_back(Cmpd);}
+	for (auto& Cmpd : equation_to_balance.left)
+	{
+		total.push_back(Cmpd);
+		if (Cmpd.mChangeSide == true) {
+			std::vector<element> dummy_vector = { element(" ", 0) }; //set dummy vector so to check for equals sign 
+			total.push_back(compound(dummy_vector));
+		}
+	}
 
 	for (auto& Cmpd : equation_to_balance.right) {total.push_back(Cmpd);}
 
 	char var = 'A';
-	for (auto& Cmpd : total) { Cmpd.mVariable = var++; }
+	for (auto& Cmpd : total) { if (Cmpd.compound_elements[0].name == " ") {} else { Cmpd.mVariable = var++; } }
 
 	std::string equation = "";
 
+	std::vector<Variable> constants;
+
+	char Solved_For = 'A';
+
+	//Find the compounds that have the the element and concat to string
 	for (size_t element = 0; element < equation_to_balance.left.size(); element++)
 	{
 		for (size_t compound = 0; compound < equation_to_balance.left[element].size; compound++)
 		{
 			for (size_t inner_compound = 0; inner_compound < total.size(); inner_compound++)
 			{
+
 				for (size_t inner_element = 0; inner_element < total[inner_compound].size; inner_element++)
 				{
+					if (total[inner_compound].compound_elements[inner_element].name == " ") {
+						equation += " = ";
+					}
+
 					if (equation_to_balance.left[element].compound_elements[compound].name == 
 						total[inner_compound].compound_elements[inner_element].name) 
 					{
+						
+
 						if (total[inner_compound].Constant != 0) {
-							equation += total[inner_compound].Constant +
-								"(" + std::to_string(total[inner_compound].compound_elements[inner_element].numb_of_elements) + ")";
+							equation += std::to_string(total[inner_compound].compound_elements[inner_element].numb_of_elements) +
+								"(" + std::to_string(total[inner_compound].Constant) + ")";
 						}
 						else {
 							equation += std::to_string(total[inner_compound].compound_elements[inner_element].numb_of_elements) + total[inner_compound].mVariable;
@@ -207,21 +236,68 @@ void balance(equation equation_to_balance) {
 				}
 			}
 
-			std::cout << equation << " ";
-			//call function to solve for constants
+			std::cout << equation << "\n";
+			//constants.push_back(getConstant(equation, Solved_For,constants));
 			equation = "";
+			//++Solved_For;
 		}
 	}
 	
 }
 
-int getConstant(std::map<int, int> equation) {
+/*
+Variable Solve_for_Variable(std::string Algebra_Equation, char Solve_for, std::vector<Variable> known_constansts) {
+
+	int num;
+	char var = ' ';
+	bool change_sides = false;
+
+	std::vector<Variable> LeftHandSide, RightHandSide;
+
+	for (size_t character = 0; character < Algebra_Equation.size(); character++){
+
+		if (Algebra_Equation.at(character) == ' ') { continue; }
+
+		if (Algebra_Equation.at(character) == '=') { change_sides = true; continue; }
+
+		if (Algebra_Equation.at(character) > 47 and Algebra_Equation.at(character) < 58)
+		{
+			std::string hold_char = " ";
+			hold_char += Algebra_Equation.at(character);
 
 
-	return 0;
+			if (Algebra_Equation.at(character + 2) > 47 and Algebra_Equation.at(character + 2) < 58) {
+				hold_char += " * " + Algebra_Equation.at(character + 2);
+			}
+			else {
+				var = Algebra_Equation.at(character + 1);
+			}
+
+			num = std::stoi(hold_char);
+
+
+			if (Algebra_Equation.at(character) == '>') {
+				RightHandSide.push_back(Variable(var, num));
+			}
+
+			if (change_sides != true) {
+				LeftHandSide.push_back(Variable(var, num));
+			}
+			else {
+				RightHandSide.push_back(Variable(var, num));
+			}
+
+			
+		}
+	}
+
+	Variable Compiler_stop_whining;
+	return Compiler_stop_whining;
 }
 
-/*
+
+
+
 		// check if the character behind it is a exponent symbol
 		if (character > 0 and line.at(character - 1) == exponent_symbol) {
 			if (line.at(character) > 47 and line.at(character) < 58) {
